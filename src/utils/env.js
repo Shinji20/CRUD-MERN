f92e7951ac296fda2e5d91999b82be2931096325
Sebastian <sebastian.urquijo@enviame.io>
2023-11-22 16:53:26 -0500

    add .env client side

--- src/utils/env.js
+++ src/utils/env.js
@@ -4,4 +4,3 @@ dotenv.config();
 
 export const CLIENT_ORIGIN = process.env["CLIENT_ORIGIN"]
 export const MONGO_DB_CONNECTION = process.env["MONGO_DB_CONNECTION"]
-export const API_URL = process.env['API_URL']
